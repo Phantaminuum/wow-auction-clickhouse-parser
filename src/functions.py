d540331b0337d8b172f3483134e089372f68071e
phant <phantaminuum@gmail.com>
2022-01-20 13:48:27 +0300

    rollback to old auth method

--- src/functions.py
+++ src/functions.py
@@ -24,6 +24,9 @@ def persist_state(state):
 def get_token():
     response = requests.post('https://us.battle.net/oauth/token', data={'grant_type': 'client_credentials'}, auth=(os.environ['WOW_CLIENT'], os.environ['WOW_SECRET']))
 
+    print(response.status_code)
+    print(response.content)
+
     return response.json()['access_token']
 
 def request_current_auctions():
@@ -31,13 +34,14 @@ def request_current_auctions():
         'base_uri': 'https://eu.api.blizzard.com/data/wow/connected-realm', 
         'realm': os.environ['WOW_REALM'], 
         'auction_id': os.environ['WOW_AUCTION_ID'],
-        'namespace': os.environ['WOW_NAMESPACE']
+        'namespace': os.environ['WOW_NAMESPACE'],
+        'token': get_token(),
     }
 
-    uri = '{base_uri}/{realm}/auctions/{auction_id}?namespace={namespace}&locale=en_US'.format(**params)
+    uri = '{base_uri}/{realm}/auctions/{auction_id}?namespace={namespace}&access_token={token}&locale=en_US'.format(**params)
     
     try:
-        response = requests.get(uri, headers={'Authorization': 'Bearer {}'.format(get_token())})
+        response = requests.get(uri)
 
         return response.json()['auctions']
 
