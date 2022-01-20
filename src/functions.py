849c0ff0ee832fe30055d610b98b7ad61da3ce77
phant <phantaminuum@gmail.com>
2022-01-20 13:41:11 +0300

    move to auto retrieve token

--- src/functions.py
+++ src/functions.py
@@ -3,6 +3,8 @@ import requests
 from clickhouse_driver import Client
 import pickle
 import os
+import time
+
 
 DUMP_FILE = '/app/data/data.dump'
 
@@ -19,18 +21,33 @@ def persist_state(state):
     with open(DUMP_FILE, 'wb') as handle:
         pickle.dump(state, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
+def get_token():
+    response = requests.post('https://us.battle.net/oauth/token', data={'grant_type': 'client_credentials'}, auth=(os.environ['WOW_CLIENT'], os.environ['WOW_SECRET']))
+
+    return response.json()['access_token']
+
 def request_current_auctions():
     params = {
         'base_uri': 'https://eu.api.blizzard.com/data/wow/connected-realm', 
         'realm': os.environ['WOW_REALM'], 
         'auction_id': os.environ['WOW_AUCTION_ID'],
-        'namespace': os.environ['WOW_NAMESPACE'],
-        'token': os.environ['WOW_TOKEN'],
+        'namespace': os.environ['WOW_NAMESPACE']
     }
-    uri = '{base_uri}/{realm}/auctions/{auction_id}?namespace={namespace}&locale=en_US&access_token={token}'.format(**params)
-    response = requests.get(uri)
 
-    return response.json()['auctions']
+    uri = '{base_uri}/{realm}/auctions/{auction_id}?namespace={namespace}&locale=en_US'.format(**params)
+    
+    try:
+        response = requests.get(uri, headers={'Authorization': 'Bearer {}'.format(get_token())})
+
+        return response.json()['auctions']
+
+    except requests.exceptions.JSONDecodeError as e:
+        print(response.status_code)
+        print(response.content)
+        time.sleep(60 * 3)
+        
+
+    # return []
 
 def persist_sold_auctions(update_list):
     client.execute(
